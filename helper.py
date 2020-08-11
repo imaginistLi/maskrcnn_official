06bce9ba0ac76699aa316d30f30504ad6dee417c
imaginist <imaginist@sjtu.edu.cn>
2020-08-11 16:52:16 +0800

    Initial commit

---
+++ helper.py
@@ -0,0 +1,14 @@
+from engine import train_one_epoch, evaluate
+import utils
+import transforms as T
+
+
+def get_transform(train):
+    transforms = []
+    # converts the image, a PIL image, into a PyTorch Tensor
+    transforms.append(T.ToTensor())
+    if train:
+        # during training, randomly flip the training images
+        # and ground-truth for data augmentation
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
